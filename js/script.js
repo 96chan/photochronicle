9b9ad9571943a8264bb2684aa5db163793470c82
chan <96chan@berkeley.edu>
2013-11-18 11:14:20 -0800

    First files

---
+++ js/script.js
@@ -0,0 +1,28 @@
+/*
+
+*** Google Map API
+https://developers.google.com/maps/documentation/javascript/tutorial#Audience
+
+
+*/
+
+var map;
+function initialize() {
+  var mapOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(-34.397, 150.644),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+}
+
+/* =====================
+
+	Document Ready
+
+====================== */
+$(document).ready(function() {
+	// init();
+    google.maps.event.addDomListener(window, "load", initialize);
+});
