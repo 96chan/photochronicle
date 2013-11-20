6d69d9643848cc67b890399a30e97eb2eff4d772
WendyXue <wendy.xue@ischool.berkeley.edu>
2013-11-20 14:25:39 -0800

    added a new view location_tours.html to display tours stops at a location

--- js/script.js
+++ js/script.js
@@ -26,4 +26,53 @@ $(document).ready(function() {
 	// init();
     google.maps.event.addDomListener(window, "load", initialize);
     $('.dropdown-toggle').dropdown();
+    $('.btn-group').button();
+
+    $('.thumbnail').on("click", function(){
+        console.log("boo");
+    });
+
+    filterStoryByTags();
 });
+
+/* ==================
+
+  Helper functions
+===================== */
+function filterStoryByTags(){
+  $("#tag-filter-dropdown li").on("click", function(){
+    var tag = $.trim($(this).text());
+      console.log(tag);
+
+    $(".story-block").each(function(index){
+      if(tag == "All Tags"){
+        $(this).show();
+      }
+      else{
+        var tags = $(this).find(".tags").text();
+        console.log(tags);
+        console.log(tags.indexOf(tag));
+        if(tags.indexOf(tag) == -1){
+          $(this).hide();
+        }
+        else{
+          $(this).show();
+        }
+      }
+    }); //story-block each function
+
+  }); //end tag-filter-dropdown click function
+}
+
+function filterStoryByInTour(){
+  
+}
+
+
+
+
+
+
+
+
+
