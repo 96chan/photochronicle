19450bb2e52141a08f64aa10f49e6bd55a75a5d7
Ignacio Perez <iaperez@gmail.com>
2013-11-27 18:22:01 -0800

    adding styles and code to manage modals

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ https://developers.google.com/maps/documentation/javascript/tutorial#Audience
 */
 
 var map;
+var map_modal;
+
 function initialize() {
   var mapOptions = {
     zoom: 8,
@@ -17,6 +19,66 @@ function initialize() {
       mapOptions);
 }
 
+function initialize_modal() {
+    var mapOptions = {
+        zoom: 8,
+        center: new google.maps.LatLng(-34.397, 150.644),
+        mapTypeId: google.maps.MapTypeId.TERRAIN
+    };
+    map_modal = new google.maps.Map(document.getElementById('mapModal'),
+        mapOptions);
+
+    // Create the search box and link it to the UI element.
+    var input = /** @type {HTMLInputElement} */(document.getElementById('pac-input'));
+    map_modal.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    var searchBox = new google.maps.places.SearchBox(/** @type {HTMLInputElement} */(input));
+    // Listen for the event fired when the user selects an item from the
+    // pick list. Retrieve the matching places for that item.
+    google.maps.event.addListener(searchBox, 'places_changed', function () {
+        var places = searchBox.getPlaces();
+
+        for (var i = 0, marker; marker = markers[i]; i++) {
+            marker.setMap(null);
+        }
+
+        // For each place, get the icon, place name, and location.
+        markers = [];
+        var bounds = new google.maps.LatLngBounds();
+        for (var i = 0, place; place = places[i]; i++) {
+            var image = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            var marker = new google.maps.Marker({
+                map: map,
+                icon: image,
+                title: place.name,
+                position: place.geometry.location
+            });
+
+            markers.push(marker);
+
+            bounds.extend(place.geometry.location);
+        }
+
+        map_modal.fitBounds(bounds);
+    });
+
+    // Bias the SearchBox results towards places that are within the bounds of the
+    // current map's viewport.
+    google.maps.event.addListener(map, 'bounds_changed', function () {
+        var bounds = map_modal.getBounds();
+        searchBox.setBounds(bounds);
+    });
+
+}
+
+
 /* =====================
 
 	Document Ready
@@ -25,8 +87,22 @@ function initialize() {
 $(document).ready(function() {
 	// init();
     google.maps.event.addDomListener(window, "load", initialize);
-    
+    google.maps.event.addDomListener(window, "load", initialize_modal);
+
 });
+
+$('#myModal').on('shown', function () {
+    //google.maps.event.addDomListener(window, "load", initialize_modal);
+    //initialize_modal();
+    google.maps.event.trigger(map_modal, "resize");
+});
+
+$('#mapModalLink').on('click', function () {
+    $("#myModal").show();
+    google.maps.event.trigger(map_modal, 'resize');
+});
+
+
 
 
 
