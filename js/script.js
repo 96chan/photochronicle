e31204719c56afe64fd0d471add4069398b148a3
chan <96chan@berkeley.edu>
2013-11-28 20:48:24 -0800

    Update SignlocalStorage

--- js/script.js
+++ js/script.js
@@ -19,64 +19,64 @@ function initialize() {
       mapOptions);
 }
 
-function initialize_modal() {
-    var mapOptions = {
-        zoom: 8,
-        center: new google.maps.LatLng(-34.397, 150.644),
-        mapTypeId: google.maps.MapTypeId.TERRAIN
-    };
-    map_modal = new google.maps.Map(document.getElementById('mapModal'),
-        mapOptions);
+// function initialize_modal() {
+//     var mapOptions = {
+//         zoom: 8,
+//         center: new google.maps.LatLng(-34.397, 150.644),
+//         mapTypeId: google.maps.MapTypeId.TERRAIN
+//     };
+//     map_modal = new google.maps.Map(document.getElementById('mapModal'),
+//         mapOptions);
 
-    // Create the search box and link it to the UI element.
-    var input = /** @type {HTMLInputElement} */(document.getElementById('pac-input'));
-    map_modal.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-    var searchBox = new google.maps.places.SearchBox(/** @type {HTMLInputElement} */(input));
-    // Listen for the event fired when the user selects an item from the
-    // pick list. Retrieve the matching places for that item.
-    google.maps.event.addListener(searchBox, 'places_changed', function () {
-        var places = searchBox.getPlaces();
+//     // Create the search box and link it to the UI element.
+//     var input = /** @type {HTMLInputElement} */(document.getElementById('pac-input'));
+//     map_modal.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+//     var searchBox = new google.maps.places.SearchBox(/** @type {HTMLInputElement} */(input));
+//     // Listen for the event fired when the user selects an item from the
+//     // pick list. Retrieve the matching places for that item.
+//     google.maps.event.addListener(searchBox, 'places_changed', function () {
+//         var places = searchBox.getPlaces();
 
-        for (var i = 0, marker; marker = markers[i]; i++) {
-            marker.setMap(null);
-        }
+//         for (var i = 0, marker; marker = markers[i]; i++) {
+//             marker.setMap(null);
+//         }
 
-        // For each place, get the icon, place name, and location.
-        markers = [];
-        var bounds = new google.maps.LatLngBounds();
-        for (var i = 0, place; place = places[i]; i++) {
-            var image = {
-                url: place.icon,
-                size: new google.maps.Size(71, 71),
-                origin: new google.maps.Point(0, 0),
-                anchor: new google.maps.Point(17, 34),
-                scaledSize: new google.maps.Size(25, 25)
-            };
+//         // For each place, get the icon, place name, and location.
+//         markers = [];
+//         var bounds = new google.maps.LatLngBounds();
+//         for (var i = 0, place; place = places[i]; i++) {
+//             var image = {
+//                 url: place.icon,
+//                 size: new google.maps.Size(71, 71),
+//                 origin: new google.maps.Point(0, 0),
+//                 anchor: new google.maps.Point(17, 34),
+//                 scaledSize: new google.maps.Size(25, 25)
+//             };
 
-            // Create a marker for each place.
-            var marker = new google.maps.Marker({
-                map: map,
-                icon: image,
-                title: place.name,
-                position: place.geometry.location
-            });
+//             // Create a marker for each place.
+//             var marker = new google.maps.Marker({
+//                 map: map,
+//                 icon: image,
+//                 title: place.name,
+//                 position: place.geometry.location
+//             });
 
-            markers.push(marker);
+//             markers.push(marker);
 
-            bounds.extend(place.geometry.location);
-        }
+//             bounds.extend(place.geometry.location);
+//         }
 
-        map_modal.fitBounds(bounds);
-    });
+//         map_modal.fitBounds(bounds);
+//     });
 
-    // Bias the SearchBox results towards places that are within the bounds of the
-    // current map's viewport.
-    google.maps.event.addListener(map, 'bounds_changed', function () {
-        var bounds = map_modal.getBounds();
-        searchBox.setBounds(bounds);
-    });
+//     // Bias the SearchBox results towards places that are within the bounds of the
+//     // current map's viewport.
+//     google.maps.event.addListener(map, 'bounds_changed', function () {
+//         var bounds = map_modal.getBounds();
+//         searchBox.setBounds(bounds);
+//     });
 
-}
+// }
 
 
 /* =====================
@@ -87,9 +87,40 @@ function initialize_modal() {
 $(document).ready(function() {
 	// init();
     google.maps.event.addDomListener(window, "load", initialize);
-    google.maps.event.addDomListener(window, "load", initialize_modal);
+    // google.maps.event.addDomListener(window, "load", initialize_modal);
 
+  // getting login name  
+  if (localStorage["name"]) {
+      $('#input-name').val(localStorage["name"]);
+      $('#signin').text('');
+      $('#after-signin').text('Welcome, '+localStorage["name"]+'!' );
+      $('#logout').text('Log out');
+  }
 });
+
+// Sign-In Function
+// Show a signin modal
+$('#signin').click(function(){
+    $('#myModal').modal('toggle');
+});
+$('#form-signin').submit(function(){
+    localStorage['name'] = $('#input-name').val();
+    $('#after-signin').text('Welcome, '+localStorage["name"]+'!');
+    $('#signin').html('');
+    $('#logout').text('Log out');
+    $('#myModal').modal('hide');
+    return false;
+});
+$('#logout').click(function(){
+    localStorage.clear();
+    $('#signin').html('<span class="glyphicon glyphicon-user"></span>&nbsp;Sign In');
+    $('#after-signin').html('');
+    $('#logout').html('');
+});
+
+
+
+
 
 $('#myModal').on('shown', function () {
     //google.maps.event.addDomListener(window, "load", initialize_modal);
@@ -101,8 +132,6 @@ $('#mapModalLink').on('click', function () {
     $("#myModal").show();
     google.maps.event.trigger(map_modal, 'resize');
 });
-
-
 
 
 
