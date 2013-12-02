e16d6c566b4adb0b25dd80781951be471f8eb0a7
chan <96chan@berkeley.edu>
2013-12-02 01:23:47 -0800

    add right panel for index.html

--- js/script_chan.js
+++ js/script_chan.js
@@ -19,11 +19,21 @@ function initialize_map_canvas() {
         zoom: 16,
         center: bk_latlng,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
-        mapTypeControl: false
+        mapTypeControl: false,
     };
-    map_canvas = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);   
+    var styles = [
+      {
+        "elementType": "labels.icon",
+        "stylers": [
+        { "visibility": "off" }
+        ]
+      },
 
-    var sg_marker = createMarker(sg_latlng, '<h2>Sather Gate</h2><p class="info-address">Sather Rd, Berkeley, CA 94720</p></br><p class="info-like">200 Likes</p> <p class="info-story">4 Stories</p><p class="info-tour">2 Tours</p><a href="#" class="info-view-more">View More</a>');
+    ];
+    map_canvas = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);   
+    map_canvas.setOptions({styles: styles});
+
+    var sg_marker = createMarker(sg_latlng, '<h2>Sather Gate</h2><p class="info-address">Sather Rd, Berkeley, CA 94720</p></br><p class="info-like">200 Likes</p> <p class="info-story">4 Stories</p><p class="info-tour">2 Tours</p><a href="location_stories.html" class="info-view-more">View More</a>');
     var st_marker = createMarker(st_latlng, '<h2>Sather Tower</h2><p class="info-address">S Hall Rd, Berkeley, CA 94720</p></br><p class="info-like">300 Likes</p> <p class="info-story">4 Stories</p><p class="info-tour">2 Tours</p><a href="#" class="info-view-more">View More</a>');
     var hgt_marker = createMarker(hgt_latlng, '<h2>Hearst Greek Theatre</h2><p class="info-address">2001 Gayley Rd, Berkeley, CA 94720</p></br><p class="info-like">500 Likes</p> <p class="info-story">4 Stories</p><p class="info-tour">2 Tours</p><a href="#" class="info-view-more">View More</a>');
     var hmmb_marker = createMarker(hmmb_latlng, '<h2>Hearst Memorial Mining Building</h2><p class="info-address">Hearst Memorial Mining Building, Berkeley, CA 94720</p></br><p class="info-like">80 Likes</p> <p class="info-story">4 Stories</p><p class="info-tour">2 Tours</p><a href="#" class="info-view-more">View More</a>');
@@ -112,9 +122,14 @@ function initialize_tour_map_canvas() {
 
 ====================== */
 $(document).ready(function() {
+  if(localStorage["intro-viewed"]){
+    $('#intro-container').hide();
+  }
   if($(document).find('#map-canvas').length > 0){
+    console.log('a');
     var height = $(document).height()-54;
     $('#map-canvas').css('height',height);
+    $('#intro-text').css('height',height);
     $('#map-canvas').css('width',$(document).width());
     // init();
     google.maps.event.addDomListener(window, "load", initialize_map_canvas);
@@ -134,29 +149,6 @@ $(document).ready(function() {
       $('#after-signin').hide();
       $('#signin').show();      
   }
-    $('#tour-grid').mixitup();
-    $("#story-grid").mixitup();
-
-    $('.dropdown-toggle').dropdown();
-    $('.btn-group').button();
-
-    $("#sort-likes").on("click", function(){
-      $('#story-grid').mixitup('sort',["data-name",'asc']);
-    });
-    // filterStoryByTags();
-    // filterStoryByInTour();
-    // filterStoryByImage(); 
-
-    
-  $(".thumbup").each(function(){
-    var storyNum = $(this).attr("for");
-    $(this).html("<span class='glyphicon glyphicon-thumbs-up'></span> "+ storyLikes[storyNum]);
-  });
-
-  $(".filter > a").on("click", function(e){
-    e.preventDefault();
-  });
-  
 });
 
 //***********************
@@ -205,6 +197,12 @@ $('#signout').click(function(){
 $('#mapModalLink').click(function(){
   $('#myModal').modal('show');
 });
+
+$('#intro-container').click(function(){
+  localStorage["intro-viewed"] = true;
+  $('#intro-container').hide();
+});
+
 //***********************
 
 
