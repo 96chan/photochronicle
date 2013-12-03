b5a8a2f685618529a82377e164d037387ac64843
chan <96chan@berkeley.edu>
2013-12-03 00:14:09 -0800

    modify mytour.html, change the color of story in new_tour_detail.html and tour_detail.html

--- js/script_new_content.js
+++ js/script_new_content.js
@@ -395,7 +395,7 @@ $('#saveLocationButton').on('click', function(event) {
 
     //markers[0].position.ob+markers[0].position.pb
     //marker.position.ob+marker.position.pb
-    $("#tourstorieslist").append("<div class=\"thumbnail\" style=\"background-color:#dddddd\"><h4>"+$("#locationReferenceName").val()+"<div class=\"no_break_float_right hide_on_load\"><small><a href=\"#\" onclick=\"$('#addStoryModal').modal(\'show\');\" id=\"mapModalLink\"><i class=\"fa fa-plus-circle\"></i> add stories</a></small></div></h4>No stories here yet! Please click on add stories...</div>");
+    $("#tourstorieslist").append("<div class=\"thumbnail each-story-container\" ><h4>"+$("#locationReferenceName").val()+"<div class=\"no_break_float_right hide_on_load\"><small><a href=\"#\" onclick=\"$('#addStoryModal').modal(\'show\');\" id=\"mapModalLink\"><i class=\"fa fa-plus-circle\"></i> add stories</a></small></div></h4>No stories here yet! Please click on add stories...</div>");
     $("#locationlist").append("<div class=\"thumbnail\"><h4>"+$("#locationReferenceName").val()+"</h4><p class=\"text-right hide_on_load\"><a href=\"#\" class=\"remove_a\">remove from tour</a></p></div>")
     $('#addnewlocation').modal('hide');
 });
